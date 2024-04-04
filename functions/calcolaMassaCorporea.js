d18f8414973ad419db7f112770e0648ebcf2c2e4
dellaquila05 <129840421+dellaquila05@users.noreply.github.com>
2024-04-04 10:02:20 +0200

    Update calcolaMassaCorporea.js

--- functions/calcolaMassaCorporea.js
+++ functions/calcolaMassaCorporea.js
@@ -1,1 +1,17 @@
+exports.handler = async (event, context) => {
+  // Estrai i valori di peso e altezza dall'oggetto event.body
+  const { peso, altezza } = JSON.parse(event.body);
 
+  // Calcola la massa corporea
+  const altezzaInMetri = altezza / 100; // Converti l'altezza da cm a metri
+  const massaCorporea = peso / (altezzaInMetri * altezzaInMetri);
+
+  // Costruisci la risposta da restituire
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({ massaCorporea })
+  };
+
+  // Restituisci la risposta
+  return response;
+};
